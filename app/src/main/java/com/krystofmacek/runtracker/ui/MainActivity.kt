c2f2db31bb7c66f4ec43e10dd5b6297ff6ad3a5c
Krystof <krystofmacek333@gmail.com>
2020-09-21 14:47:12 +0200

    Navigation + Layouts

--- app/src/main/java/com/krystofmacek/runtracker/ui/MainActivity.kt
+++ app/src/main/java/com/krystofmacek/runtracker/ui/MainActivity.kt
@@ -2,11 +2,32 @@ package com.krystofmacek.runtracker.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.krystofmacek.runtracker.R
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // setup navigation
+        setSupportActionBar(toolbar)
+
+        val navController = navHostFragment.findNavController()
+        bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when(destination.id) {
+                R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                    bottomNavigationView.visibility = View.VISIBLE
+
+                else -> bottomNavigationView.visibility = View.GONE
+            }
+        }
     }
 }
--- app/src/main/java/com/krystofmacek/runtracker/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/krystofmacek/runtracker/ui/fragments/SetupFragment.kt
@@ -1,13 +1,19 @@
 package com.krystofmacek.runtracker.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.krystofmacek.runtracker.R
+import kotlinx.android.synthetic.main.fragment_setup.*
 
 
 class SetupFragment : Fragment(R.layout.fragment_setup) {
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        tvContinue.setOnClickListener{
+            findNavController().navigate(R.id.action_setupFragment_to_runFragment3)
+        }
+    }
 }
